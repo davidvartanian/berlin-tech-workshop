a3129ef2ebf5d04b26cdf9f230509cb03afb9b0b
David Vartanian <david.vartanian@mcmakler.de>
2019-03-29 17:26:12 +0100

    Add support for private links and fix auth tests

--- linkshortener/shortener/models.py
+++ linkshortener/shortener/models.py
@@ -7,5 +7,5 @@ class Link(models.Model):
     name = models.CharField(max_length=255)
     token = models.CharField(max_length=10, null=False, unique=True)
     url = models.CharField(max_length=1024, null=False)
-
+    private = models.BooleanField(default=False, null=False)
     owner = models.ForeignKey(User, related_name="links", on_delete=models.CASCADE, null=True)
--- linkshortener/shortener/views.py
+++ linkshortener/shortener/views.py
@@ -1,6 +1,4 @@
-import logging
-
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import viewsets
@@ -25,5 +23,8 @@ class LinkViewSet(viewsets.ModelViewSet):
 @permission_classes((AllowAny, ))
 def redirect_view(request, token):
     link = get_object_or_404(Link, token=token)
+    current_user = request.user
+    if link.private and current_user != link.owner:
+        raise Http404('Link does not exist or you don\'t have access to it.')
     response = redirect(link.url)
     return response
